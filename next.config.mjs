4f9e3cd85d2d1247bf6a0f1e92a54191ff1276ae
Julian <julian.kailich@udarealestate.com>
2024-08-21 16:53:13 -0300

    Fix config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    trailingSlash: true,
-  };
-  
-  export default nextConfig;
+  trailingSlash: true,
+  exportTrailingSlash: true,
+  async exportPathMap(defaultPathMap) {
+    return {
+      "/": { page: "/" },
+    };
+  },
+};
+
+export default nextConfig;
