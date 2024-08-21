22bf5a98e0129ed140539d3fe0ee755f8f817d32
Julian <julian.kailich@udarealestate.com>
2024-08-21 16:43:50 -0300

    Delete unnecessary model

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,6 +1,4 @@
-import { PlaceholdersConfig } from "./model/placeholders/placeholders.model";
-
-export const SEARCH_TYPE_PLACEHOLDERS: PlaceholdersConfig = {
+export const SEARCH_TYPE_PLACEHOLDERS: any = {
   name: "Search by country name",
   code: "Search by ISO code",
   continent: "Search by continent CODE (ex. South America: SA)",
--- src/shared/model/placeholders/placeholders.model.tsx
+++
@@ -1,5 +0,0 @@
-export interface PlaceholdersConfig {
-    name: string;
-    code: string;
-    continent: string;
-}
