997809d06e71463961f974b89fae29094541a67f
Julian <julian.kailich@udarealestate.com>
2024-08-21 17:03:36 -0300

    Vercel config

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  trailingSlash: true,
-  exportTrailingSlash: true,
-  async exportPathMap(defaultPathMap) {
-    return {
-      "/": { page: "/" },
-    };
-  },
-};
-
-export default nextConfig;
+    trailingSlash: true,
+  };
+  
+  export default nextConfig;
